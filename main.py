12e01ea9106d0dff1002feee40424f8611dfdd49
Nivrito <nivrita540@gmail.com>
2021-09-02 02:10:18 +0600

    Adding database and layout.html

--- main.py
+++ main.py
@@ -6,16 +6,22 @@ params = json.load(open('config.json'))['params']
 print(params)
 app.config['SQLALCHEMY_DATABASE_URI'] = params['data_base_uri'] 
 db = SQLAlchemy(app)
-class movies(db.Model): 
+class Movies(db.Model): 
     sno = db.Column(db.Integer, primary_key=True) 
     name = db.Column(db.String(20), nullable=False) 
-    description = db.Column(db.String(200), nullable=False)
     slug =  db.Column(db.String(20), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    genre = db.Column(db.String(200), nullable=False)
     img_name =  db.Column(db.String(20), nullable=False)
-    datatime =  db.Column(db.String(20), nullable=False)
+    date =  db.Column(db.String(20), nullable=False)
 # slug =  db.Column(db.String(20), nullable=False)
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def home():
+    movies = Movies.query.all()
+    return render_template("index.html", movies=movies)
+@app.route("/<string:slug>")
+def movie_download(slug):
+    movie = Movies.query.filter_by(slug=slug).first()
+    return render_template("test.html", movie=movie)
 
 app.run(debug=True)
