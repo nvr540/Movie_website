2c20a6834d90addc3d47930f8f436319f6355ad8
Nivrito <nivrita540@gmail.com>
2021-09-08 21:15:39 +0600

    Just done some work with sorting

--- main.py
+++ main.py
@@ -13,18 +13,28 @@ class Movies(db.Model):
     description = db.Column(db.String(200), nullable=False)
     genre = db.Column(db.String(200), nullable=False)
     img_name =  db.Column(db.String(20), nullable=False)
+    film_industry =  db.Column(db.String(20), nullable=False)
     date =  db.Column(db.String(20), nullable=False)
 # slug =  db.Column(db.String(20), nullable=False)
 @app.route("/")
 def home():
-    movies = Movies.query.all()
+    # movies = Movies.query.all()
     latest = Movies.query.order_by(Movies.date.desc()).limit(7).all()
-    comedy = Movies.query.filter_by(genre='comedy').order_by(Movies.date.desc()).all()
-    horror = Movies.query.filter_by(genre='horror').order_by(Movies.date.desc()).all()
-    action = Movies.query.filter_by(genre='action').order_by(Movies.date.desc()).all()
-    romance = Movies.query.filter_by(genre='romance').order_by(Movies.date.desc()).all()
-    return render_template("index.html", movies=movies,latest=latest, comedy=comedy,horror=horror, action=action,romance=romance)
-@app.route("/<string:slug>")
+    catagory = request.args.get('catagory') #Have to make catagory wise selection.
+    if catagory == "film":
+        action = Movies.query.filter_by(film_industry = 'anime').order_by(Movies.date.desc()).all()
+        romance = Movies.query.filter_by(film_industry = 'hollywood').order_by(Movies.date.desc()).all()
+        horror = Movies.query.filter_by(film_industry = 'bollywood').order_by(Movies.date.desc()).all()
+        comedy = Movies.query.filter(Movies.film_industry != 'bollywood').filter(Movies.film_industry !='hollywood').filter(Movies.film_industry != 'anime').order_by(Movies.date.desc()).all()
+        first_row,second_row, third_row, fourth_row = ["Anime","Hollywood","Bollywood","Others"]
+    else:
+        action = Movies.query.filter_by(genre='action').order_by(Movies.date.desc()).all()
+        romance = Movies.query.filter_by(genre='romance').order_by(Movies.date.desc()).all()
+        horror = Movies.query.filter_by(genre='horror').order_by(Movies.date.desc()).all()
+        comedy = Movies.query.filter_by(genre='comedy').order_by(Movies.date.desc()).all()
+        first_row,second_row, third_row, fourth_row = ["Action","Romance","Horror","Comedy"]
+    return render_template("index.html",latest=latest, comedy=comedy,horror=horror, action=action,romance=romance, first_row = first_row, second_row = second_row, third_row=third_row, fourth_row=fourth_row)
+@app.route("/movies/<string:slug>")
 def movie_download(slug):
     movie = Movies.query.filter_by(slug=slug).first()
     return render_template("test.html", movie=movie)
